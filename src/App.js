0f7022e18bbfcb61bcbde19944f2f371604853b4
maduonyekaemeka33 <maduonyekaemeka@gmail.com>
2026-02-01 07:04:58 +0100

    Add App.js with JupiterSwap component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JupiterSwap from './components/JupiterSwap';
+
+function App() {
+  return (
+    <div>
+      <h1>New Coin Finder</h1>
+      <JupiterSwap />
+    </div>
+  );
+}
+
+export default App;
